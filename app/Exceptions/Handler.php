3d2f0ee6724fd235b424d9c1558a11526044cb64
Arun  A S <arunachu.as@gmail.com>
2023-06-13 19:31:05 +0530

    Handle errors globally

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,20 @@
 
 namespace App\Exceptions;
 
+use App\Enums\ErrorCodes;
+use App\Traits\JsonResponses;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use JsonResponses;
+
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -26,5 +35,60 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        /** @var \Illuminate\Http\Request $request */
+        if ($request->wantsJson() || request()->is('api/*')) {
+            return $this->convertExceptionToJsonResponse($e);
+        }
+        return parent::render($request, $e);
+    }
+
+    /**
+     * Converts the exception to a json response
+     *
+     * @param \Throwable $e
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function convertExceptionToJsonResponse(Throwable $e)
+    {
+        $message = $e->getMessage();
+        $statusCode = 500;
+        $errors = [];
+        $errorCode = ErrorCodes::INTERNAL_SERVER_ERROR;
+        $headers = [];
+        if ($e instanceof ValidationException) {
+            $statusCode = 422;
+            $errors = $e->validator->errors()->toArray();
+            $message = $e->validator->errors()->first() ?? 'Invalid request data';
+            $errorCode = ErrorCodes::VALIDATION_ERROR;
+        } else if ($e instanceof ModelNotFoundException) {
+            $statusCode = 404;
+            $message = 'Resource not Found';
+            $errorCode = ErrorCodes::RESOURCE_NOT_FOUND;
+        } elseif ($e instanceof HttpExceptionInterface) {
+            // If the exception is an instance of HtppException, then it may have headers
+            // which were assigned by framework or by us
+            $headers = $e->getHeaders();
+            $statusCode = $e->getStatusCode();
+            if ($e instanceof NotFoundHttpException) {
+                $message = "Route not found";
+                $errorCode = ErrorCodes::ROUTE_NOT_FOUND;
+            } elseif ($e instanceof TooManyRequestsHttpException) {
+                $errorCode = ErrorCodes::TOO_MANY_REQUESTS;
+            } elseif ($statusCode == 503) {
+                $message = "Server Under Maintenance. Please try again after some time";
+                $errorCode = ErrorCodes::UNDER_MAINTENANCE;
+            } else {
+                $errorCode = null;
+            }
+        }
+        if (app()->isProduction() && $statusCode == 500) {
+            $message = "Technical Error. Please try again later";
+        }
+        return $this->sendError($message, $statusCode, $errors, $errorCode, $headers);
     }
 }
--- app/Traits/JsonResponses.php
+++ app/Traits/JsonResponses.php
@@ -2,33 +2,52 @@
 
 namespace App\Traits;
 
+use App\Enums\ErrorCodes;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Http\JsonResponse;
 use JsonSerializable;
 use Stringable;
+use Throwable;
 
 trait JsonResponses
 {
     public function sendResponse(
         string|array|Stringable|Arrayable|JsonSerializable $data = [],
         int $statusCode = 200,
-        string $message = 'Success'
+        string $message = 'Success',
+        array $headers = []
     ): JsonResponse {
         return $this->response(
             $statusCode,
             $data,
-            $message
+            $message,
+            headers: $headers
         );
     }
 
     public function sendError(
+        string|Throwable $message = 'Technical Error. Please try again later',
         int $statusCode = 500,
-        string $message = 'Technical Error. Please try again later',
         string|array|Stringable|Arrayable|JsonSerializable $errors = [],
-        string $errorCode = '',
+        ?ErrorCodes $errorCode = null,
+        array $headers = []
 
     ): JsonResponse {
-        return $this->response($statusCode, message: $message, errors: $errors, errorCode: $errorCode);
+
+        if ($message instanceof Throwable) {
+            // If it is an instance of throwable, we will throw the exception.
+            // Our exception handler will then convert the exception to message, statusCode, etc
+            // and call this function again.
+            throw $message;
+        }
+
+        return $this->response(
+            $statusCode,
+            message: $message,
+            errors: $errors,
+            errorCode: $errorCode,
+            headers: $headers
+        );
     }
 
     private function response(
@@ -36,13 +55,18 @@ trait JsonResponses
         string|array|Stringable|Arrayable|JsonSerializable $data = [],
         string $message = 'Success',
         string|array|Stringable|Arrayable|JsonSerializable $errors = [],
-        string $errorCode = '',
+        ?ErrorCodes $errorCode = null,
+        array $headers = []
     ): JsonResponse {
-        return response()->json([
-            'data' => $data,
-            'message' => $message,
-            'errors' => $errors,
-            'error_code' => $errorCode,
-        ], $statusCode);
+        return response()->json(
+            [
+                'data' => $data,
+                'message' => $message,
+                'errors' => $errors,
+                'error_code' => $errorCode,
+            ],
+            $statusCode
+        )
+            ->withHeaders($headers);
     }
 }
